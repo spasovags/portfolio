318975c488e818c7ef98349ee356c12af016c98f
Spasova <eexemplega@gmail.com>
2022-11-22 15:00:15 +0200

    anim

--- components/TechStack.js
+++ components/TechStack.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Technology from '../components/Technology'
 import styles from '../styles/TechStack.module.css'
+import { useInView } from 'react-intersection-observer';
 
 
 const TechStack = ({technologies}) => {
-  
+
    return (
-    <section className={`${styles["techstack-container"]}`} >
-       {technologies.map((technology) =>
-        <Technology key={technology.id} value={technology}/>)}
+    <section className={`${styles["techstack-container"]}`}>
+       {technologies.map((technology, index) =>
+        <Technology key={technology.id} indexAnim={index} value={technology}/>)}
     </section>
   )
 }
--- components/Technology.js
+++ components/Technology.js
@@ -5,21 +5,26 @@ import { useInView } from 'react-intersection-observer';
 
 const Technology = (props) => {
   const post = props.value;
+  const leftAnim = "leftAnim";
+  const rightAnim = "rightAnim";
+
+  let anim = props.indexAnim%2==0?leftAnim:rightAnim;
   const basePath = '/images/techstack'
   const { ref: cardRef, inView: IsCardsVisible } = useInView({triggerOnce: true});
-  const { ref: shakeRef, inView: IsShakenVisible } = useInView({triggerOnce: true});
 
    return (
       <div 
-      className={`${styles["card"]} ${IsCardsVisible ? styles.animCard : ""}`}
+      className={`${styles["card"]} ${IsCardsVisible ? `${styles[`${anim}`]}` : ""}`}
       ref={cardRef} >
-         <div className={`${styles["photoCard"]}`}>
+         <div>
+         <div className={`${styles["photoCard"]}`} >
           <Image 
           src={`${basePath}/${post.photo}`} 
           layout="fill"
           objectFit='contain'
           alt={post.alt}/>
-        </div>       
+        </div> 
+        </div>      
       </div>
    );
 }
