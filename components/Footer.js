7b766f5dab603a4cab9c8c6b0dfa8c582aa9340e
Spasova <eexemplega@gmail.com>
2023-01-05 15:38:34 +0200

    footer changed

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import styles from '../styles/Footer.module.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+// import the icons you need
+import {
+  faLinkedin,
+  faGithub,
+  faYoutube,
+  faDev
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={`${styles["footer-container"]}`} >
+      <div className={`${styles["footer-sm-icons"]}`} >
+       <FontAwesomeIcon
+        icon={faYoutube}/>
+       <FontAwesomeIcon
+        icon={faGithub}/>
+       <FontAwesomeIcon
+        icon={faLinkedin}/>
+        <FontAwesomeIcon
+        icon={faDev}/>      
+      </div> 
+      <div className={`${styles["footer-info"]}`} >
+      
+      </div>     
+    </div>      
   )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
+import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 export default function MyApp({ Component, pageProps }) {
   return (
